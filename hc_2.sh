021ae4377540f53faa350f06fadbf1834d16af59
Swathi <id>
2023-09-09 15:51:56 +0000

    forth commit

--- hc_2.sh
+++ hc_2.sh
@@ -1,5 +1,42 @@
-#!bin/bash
+#!/bin/bash
 echo "HTTPS Status Code"
+
 curl -I https://www.example.com
 
+echo -e "\n"
 
+echo "Response Time"
+
+time curl -o/dev/null-s https://www.example.com
+
+echo -e "\n"
+
+echo "Query DNS Servers"
+
+nslookup google.com
+
+echo -e "\n"
+
+echo "Domain Information Groper"
+
+dig google.com
+
+echo -e "\n"
+
+echo "Perform DNS Lookups"
+
+host microsoft.com
+
+echo -e "\n"
+
+echo "DNS resolution by sending ICMP Echo"
+
+ping google.com
+
+echo -e "\n"
+
+echo "Broken Links"
+
+wget --spider --recursive --level=1 https:/www.example.com
+
+echo -e "\n"
